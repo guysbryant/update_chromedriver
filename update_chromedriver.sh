c5d988e152ef6d62672ec97b4355bf1afc6ef01e
Guy Bryant <guy.s.bryant@gmail.com>
2023-08-29 12:41:42 -0400

    Add architecture handling so the script works on Intel and Arm

--- update_chromedriver.sh
+++ update_chromedriver.sh
@@ -5,6 +5,26 @@ command_exists() {
   command -v "$1" >/dev/null 2>&1
 }
 
+# Function to get the architecture of the current system
+get_architecture() {
+  uname -m
+}
+
+# Function to determine the appropriate ChromeDriver URL basd on the architecture
+get_chrome_driver_url() {
+  local ARCHITECTURE="$1"
+  local LATEST_VERSION="$2"
+
+  if [[ "$ARCHITECTURE" == "x86_64" ]]; then
+    echo "https://chromedriver.storage.googleapis.com/$LATEST_VERSION/chromedriver_mac64.zip"
+  elif [[ "$ARCHITECTURE" == "arm64" ]]; then
+    echo "https://chromedriver.storage.googleapis.com/$LATEST_VERSION/chromedriver_mac_arm64.zip"
+  else
+    echo "Error: Unsupported architecture: $ARCHITECTURE"
+    exit 1
+  fi
+}
+
 # Function to get the latest version of ChromeDriver
 get_latest_version() {
   LATEST_VERSION=$(curl -sS "https://chromedriver.storage.googleapis.com/LATEST_RELEASE")
@@ -13,29 +33,27 @@ get_latest_version() {
 
 # Function to download and install the latest version of ChromeDriver
 install_latest_version() {
-  LATEST_VERSION=$(get_latest_version)
-  if [[ -n "$LATEST_VERSION" ]]; then
-    CHROME_DRIVER_URL="https://chromedriver.storage.googleapis.com/$LATEST_VERSION/chromedriver_mac64.zip"
-    TMP_DIR=$(mktemp -d)
-    
-    echo "Downloading ChromeDriver version $LATEST_VERSION..."
-    curl -L "$CHROME_DRIVER_URL" -o "$TMP_DIR/chromedriver_mac64.zip"
-    
-    if [ $? -eq 0 ]; then
-      echo "Extracting..."
-      unzip -q "$TMP_DIR/chromedriver_mac64.zip" -d "$TMP_DIR"
-      sudo mv "$TMP_DIR/chromedriver" "/usr/local/bin/chromedriver"
-      sudo chown root:wheel "/usr/local/bin/chromedriver"
-      sudo chmod +x "/usr/local/bin/chromedriver"
-      echo "ChromeDriver updated successfully!"
-    else
-      echo "Error: Failed to download ChromeDriver."
-    fi
-    
-    rm -rf "$TMP_DIR"
+  local LATEST_VERSION=$(get_latest_version)
+  local ARCHITECTURE=$(get_architecture)
+
+  local CHROME_DRIVER_URL=$(get_chrome_driver_url "$ARCHITECTURE" "$LATEST_VERSION")
+  local TMPDIR=$(mktemp -d)
+
+  echo "Downloading ChromeDriver version $LATEST_VERSION..."
+  curl -L "$CHROME_DRIVER_URL" -o "$TMP_DIR/chromedriver.zip"
+
+  if [ $? -eq 0 ]; then
+    echo "Extracting..."
+    unzip -q "$TMP_DIR/chromedriver.zip" -d "$TMP_DIR"
+    sudo mv "$TMP_DIR/chromedriver" "/usr/local/bin/chromedriver"
+    sudo chown root:wheel "/usr/local/bin/chromedriver"
+    sudo chmod +x "/usr/local/bin/chromedriver"
+    echo "ChromeDriver updated successfully!"
   else
-    echo "Error: Could not get the latest version of ChromeDriver."
+    echo "Error: Failed to download ChromeDriver."
   fi
+
+  rm -rf "$TMP_DIR"
 }
 
 # Check if curl and unzip commands are available
